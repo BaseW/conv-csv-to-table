4cc19349473140368ba9ca5f911ff79f7c08bf29
wada <wadayugo@keio.jp>
2022-10-27 11:48:44 +0900

    read csv from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::error::Error;
+use std::io;
+use std::process;
+
+fn read_csv_from_stdin() -> Result<(), Box<dyn Error>> {
+    // Build the CSV reader and iterate over each record.
+    let mut rdr = csv::Reader::from_reader(io::stdin());
+    for result in rdr.records() {
+        // The iterator yields Result<StringRecord, Error>, so we check the
+        // error here.
+        let record = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = read_csv_from_stdin() {
+        println!("error running example: {}", err);
+        process::exit(1);
+    }
 }
