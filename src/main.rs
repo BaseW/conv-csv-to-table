15ae2017db752dc64f18fd4d823b5226d60b3ebf
wada <wadayugo@keio.jp>
2022-10-27 12:00:15 +0900

    split functions

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,31 @@
+use csv::StringRecord;
 use std::error::Error;
 use std::io;
 use std::process;
 
-fn read_csv_from_stdin() -> Result<(), Box<dyn Error>> {
+fn read_csv_from_stdin<'a>() -> Vec<Result<StringRecord, csv::Error>> {
     // Build the CSV reader and iterate over each record.
     let mut rdr = csv::Reader::from_reader(io::stdin());
-    for result in rdr.records() {
-        // The iterator yields Result<StringRecord, Error>, so we check the
-        // error here.
+    rdr.records().collect::<Vec<_>>()
+}
+
+fn convert_csv_to_table(
+    records: Vec<Result<StringRecord, csv::Error>>,
+) -> Result<String, Box<dyn Error>> {
+    for result in records {
         let record = result?;
         println!("{:?}", record);
     }
-    Ok(())
+    Ok("ok".to_string())
+}
+
+fn get_table_from_stdin() -> Result<String, Box<dyn Error>> {
+    let records = read_csv_from_stdin();
+    convert_csv_to_table(records)
 }
 
 fn main() {
-    if let Err(err) = read_csv_from_stdin() {
+    if let Err(err) = get_table_from_stdin() {
         println!("error running example: {}", err);
         process::exit(1);
     }
