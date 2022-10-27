974822ff755f630ee727a1ab4a6a4ec3b498adea
wada <wadayugo@keio.jp>
2022-10-27 12:22:07 +0900

    implement conversion process

--- src/main.rs
+++ src/main.rs
@@ -12,11 +12,67 @@ fn read_csv_from_stdin<'a>() -> Vec<Result<StringRecord, csv::Error>> {
 fn convert_csv_to_table(
     records: Vec<Result<StringRecord, csv::Error>>,
 ) -> Result<String, Box<dyn Error>> {
-    for result in records {
+    let mut table = String::new();
+
+    for (row_index, result) in records.into_iter().enumerate() {
+        // 各行の処理開始
         let record = result?;
-        println!("{:?}", record);
+        let mut row = String::new();
+
+        // FIXME: 最初の行以外では使わない値
+        let mut col_count = 0;
+
+        for (col_index, field) in record.into_iter().enumerate() {
+            // 各列の処理開始
+            let mut col = String::new();
+
+            // FIXME: 最初の行以外では使わない値
+            col_count = col_count + 1;
+
+            if col_index == 0 {
+                // 最初の列だけ処理を変える
+                col.push_str(&format!("| {} |", field));
+            } else {
+                // 最初以外の列の処理
+                col.push_str(&format!(" {} |", field));
+            }
+
+            row.push_str(&col);
+        }
+
+        if row_index == 0 {
+            // 最初の行のデータを追加する前に、仮のヘッダーを追加する
+            for col_index in 0..col_count {
+                if col_index == 0 {
+                    // 最初の列だけ処理を変える
+                    table.push_str("| column1 |");
+                } else {
+                    table.push_str(&format!(" column{} |", col_index + 1));
+                }
+            }
+            // 改行を追加
+            table.push('\n');
+
+            for col_index in 0..col_count {
+                if col_index == 0 {
+                    // 最初の列だけ処理を変える
+                    table.push_str("| --- |");
+                } else {
+                    table.push_str(" --- |");
+                }
+            }
+            // 改行を追加
+            table.push('\n');
+        }
+
+        // 各行のデータを追加
+        table.push_str(&row);
+
+        // 改行を追加
+        table.push('\n');
     }
-    Ok("ok".to_string())
+
+    Ok(table)
 }
 
 fn get_table_from_stdin() -> Result<String, Box<dyn Error>> {
@@ -25,8 +81,13 @@ fn get_table_from_stdin() -> Result<String, Box<dyn Error>> {
 }
 
 fn main() {
-    if let Err(err) = get_table_from_stdin() {
-        println!("error running example: {}", err);
-        process::exit(1);
+    match get_table_from_stdin() {
+        Ok(table) => {
+            println!("{}", table);
+        }
+        Err(err) => {
+            println!("error running example: {}", err);
+            process::exit(1);
+        }
     }
 }
